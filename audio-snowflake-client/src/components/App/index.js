9fa0b977ed9e4a2209080bcd8137593f6159cf2f
Wendy Dherin <wendydherin@gmail.com>
2020-04-18 18:56:10 +0200

    Remove React header.

--- audio-snowflake-client/src/components/App/index.js
+++ audio-snowflake-client/src/components/App/index.js
@@ -17,18 +17,7 @@ export default class App extends Component {
     return  (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Audio Snowflake Header
         </header>
         <P5Wrapper
               className="P5-wrapper"
