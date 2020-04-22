2c44518d6640ebda52f78e4ad2526bb0683eb82e
Wendy Dherin <wendydherin@gmail.com>
2020-04-22 12:49:38 +0200

    Install react-apollo to the dev dependences. Remove old info from manifest.json. Wrap app in Apollo Provider and create Apollo cache.

--- audio-snowflake-client/src/index.js
+++ audio-snowflake-client/src/index.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloProvider } from 'react-apollo'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+
 import './index.scss'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
+
+const cache = new InMemoryCache()
+const link = new HttpLink({
+  uri: 'http://localhost:4000/',
+  headers: {
+    authorization: localStorage.getItem('token'),
+  },
+})
+
+const client = new ApolloClient({
+  cache,
+  link,
+})
+
+cache.writeData({
+  data: {
+    isAuthenticated: !!localStorage.getItem('token')
+  },
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
