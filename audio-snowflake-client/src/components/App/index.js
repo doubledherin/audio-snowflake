91f0aba766112700951ec4987d150078b4fb3db8
Wendy Dherin <wendydherin@gmail.com>
2020-04-22 12:27:57 +0200

    Get rid of Bootstrap.

--- audio-snowflake-client/src/components/App/index.js
+++ audio-snowflake-client/src/components/App/index.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react'
 import P5Wrapper from "../P5Wrapper/"
 import './index.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navigation from '../Navigation'
-import InputForm from '../InputForm'
+// import Navigation from '../Navigation'
+// import InputForm from '../InputForm'
 import Player from '../Player'
 
 export default class App extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      p5Props: {
-        status: ""
-      },
-    }
-  }
+  
   render() {
     return  (
       <div className="App bg-black container-fluid">
-        <Navigation />
         <div id="main">
           <P5Wrapper className="P5-wrapper" />
-          <InputForm />
           <Player />
         </div>
       </div>
