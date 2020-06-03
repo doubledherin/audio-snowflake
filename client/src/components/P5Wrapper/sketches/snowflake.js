02b8490827492e96d79ad2dc613b2b50dbd9e801
Wendy Dherin <wendydherin@gmail.com>
2020-06-03 13:58:06 +0200

    Clean up snowflake sketch and remove perlinNoise flower sketch.

--- client/src/components/P5Wrapper/sketches/snowflake.js
+++ client/src/components/P5Wrapper/sketches/snowflake.js
@@ -1,84 +1,38 @@
 import { getHypotrochoid } from '../../../utils'
 import { snowflakeDataOfEvil } from '../../../tests/__fixtures'
 
-/* globals */
-
 export default function sketch(p) {
 
-  p.onSetAppState = () => {}
-  // Methods -------------------------------------------------------------------
-
-
-  // Private members -----------------------------------------------------------
-  
   let hypotrochoids = []
   let angle = 0.0
   let canvasWidth
 
-  // class Pattern {
-  //   constructor() {
-  //     this.colorMode = (p.HSB)
-  //   }
-  // }
-  
-  // Private classes -----------------------------------------------------------
-  // class Hypotrochoid extends Pattern {
-  //   constructor(statorRadius, rotorRadius, distanceFromRotorCenter, hue, saturation, brightness, opacity) {
-  //     super()
-  //     this.statorRadius = statorRadius
-  //     this.rotorRadius = rotorRadius
-  //     this.distanceFromRotorCenter = distanceFromRotorCenter
-  //     this.hue = hue
-  //     this.saturation = saturation
-  //     this.brightness = brightness
-  //     this.opacity = opacity
-  //   }
-  // }
-
-  // Lifecycle methods =========================================================
-  // Make async calls to server here
-  // preload() -----------------------------------------------------------------
-  p.preload = function() {}
-  
-
-  // setup() -------------------------------------------------------------------
-  p.setup = function() {
-    // console.log("::: setup() p:", p)
+  p.setup = () => {
     canvasWidth = p.min(p.windowWidth, p.windowHeight) - 200
+    p.colorMode(p.HSB)
     p.createCanvas(canvasWidth, canvasWidth)
     p.smooth()
-    const { duration, energy, valence } = snowflakeDataOfEvil
-    
+    const { duration, energy, valence } = snowflakeDataOfEvil    
     hypotrochoids = snowflakeDataOfEvil.sections.map(section => {
       return getHypotrochoid(duration, energy, valence, section)
     })
   }
 
-  // draw() --------------------------------------------------------------------
-  p.draw = function() {
-    p.colorMode(p.HSB)
+  p.draw = () => hypotrochoids.forEach(drawHypotrochoid)
 
-    // console.log("::: draw() props:", props)
-    for (let i = 0; i < hypotrochoids.length; i++) {
-      const { statorRadius, rotorRadius, penDistance, hue, saturation, brightness, opacity } = hypotrochoids[i]
-      hypotrochoid(statorRadius, rotorRadius, penDistance, hue, saturation, brightness, opacity)
-    }
-  }
-
-  p.windowResized = function() {
+  p.windowResized = () => {
     canvasWidth = p.min(p.windowWidth, p.windowHeight)
     p.resizeCanvas(canvasWidth, canvasWidth);
   }
 
-  function hypotrochoid(statorRadius, rotorRadius, penDistance, hue, saturation, brightness, opacity) {
-    let x = ((statorRadius - rotorRadius) * Math.cos(angle)) + (penDistance * Math.cos(((statorRadius - rotorRadius) / rotorRadius) * angle)) + 300
-    let y = ((statorRadius - rotorRadius) * Math.sin(angle)) - (penDistance * Math.sin(((statorRadius - rotorRadius) / rotorRadius) * angle)) + 300
-    p.translate(0, 0)
-    p.noStroke()
+  function drawHypotrochoid({ statorRadius, rotorRadius, penDistance, hue, saturation, brightness, opacity }) {
+    p.push()
+    p.translate(300, 300) // TODO: Figure out how to really center this
+    let x = ((statorRadius - rotorRadius) * Math.cos(angle)) + (penDistance * Math.cos(((statorRadius - rotorRadius) / rotorRadius) * angle))
+    let y = ((statorRadius - rotorRadius) * Math.sin(angle)) - (penDistance * Math.sin(((statorRadius - rotorRadius) / rotorRadius) * angle))
     p.stroke(hue, saturation, brightness, opacity)
-    // p.scale(.4)
     p.ellipse(x, y, 2, 2)
-    // p.scale(2.5)
     angle += 0.00111
+    p.pop()
   }
 }
