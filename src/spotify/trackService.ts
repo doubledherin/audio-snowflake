cb412f37d8703ee808a2863d99b6d389d38da821
Wendy Dherin <wendydherin@gmail.com>
2024-11-04 09:36:17 -0800

    Clean up getTrackId in trackService.

--- src/spotify/trackService.ts
+++ src/spotify/trackService.ts
@@ -1,28 +1,7 @@
 import { getToken } from "./tokenService";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const tokenFilePath = path.join(__dirname, "token.json");
-
-// QUESTION: Pass token as a parameter?
 async function getTrackId(trackName: String, trackArtist: String) {
-  let accessToken = "";
-  let tokenExpiryTime = 0;
-
-  if (!accessToken || Date.now() >= tokenExpiryTime) {
-    try {
-      accessToken = await getToken();
-      const tokenData = JSON.parse(fs.readFileSync(tokenFilePath, "utf-8"));
-      accessToken = tokenData.accessToken;
-      tokenExpiryTime = tokenData.tokenExpiryTime;
-    } catch (error) {
-      console.error("Error getting token:", error);
-      return;
-    }
-  }
+  const accessToken = await getToken();
 
   try {
     const response = await fetch(
